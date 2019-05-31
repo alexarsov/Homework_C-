c851fc6ecf37ab143716536193c78bf253399cea
alexarsov <51236703+alexarsov@users.noreply.github.com>
2019-05-31 16:30:38 +0300

    homework

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class Node
+        {
+            public int data;
+            public Node left, right;
+
+            public Node(int data)
+            {
+                this.data = data;
+                left = right = null;
+            }
+        }
+
+        public class BinaryTree
+        {
+            public Node root;
+
+            public static int findMax(Node node)
+            {
+                if (node == null)
+                {
+                    return int.MinValue;
+                }
+
+                int res = node.data;
+                int firstCheckres = findMax(node.left);
+                int secondCheckres = findMax(node.right);
+
+                if (firstCheckres > res)
+                {
+                    res = firstCheckres;
+                }
+                if (secondCheckres > res)
+                {
+                    res = secondCheckres;
+                }
+                return res;
+            }
+
+            static void Main(string[] args)
+            {
+                BinaryTree tree = new BinaryTree();
+                tree.root = new Node(10);
+                tree.root.left = new Node(20);
+                tree.root.right = new Node(30);
+                tree.root.left.right = new Node(40);
+                tree.root.left.right.left = new Node(50);
+                tree.root.left.right.right = new Node(60);
+                tree.root.right.right = new Node(70);
+                tree.root.right.right.left = new Node(99);
+
+                Console.WriteLine("Max element is " + BinaryTree.findMax(tree.root));
+            }
+        }
+    }
+}
---
+++ minValMaxValTree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minValMaxValTree
+{
+    class Program
+    {
+        public class Node
+        {
+            public int data;
+            public Node left, right;
+
+            public Node(int d)
+            {
+                data = d;
+                left = right = null;
+            }
+        }
+
+        public class BinaryTree
+        {
+
+            public static Node head;
+
+            public virtual Node insert(Node node, int data)
+            {
+                if (node == null)
+                {
+                    return (new Node(data));
+                }
+                else
+                {
+                    if (data <= node.data)
+                    {
+                        node.left = insert(node.left, data);
+                    }
+                    else
+                    {
+                        node.right = insert(node.right, data);
+                    }
+                    return node;
+                }
+            }
+
+            public virtual int minvalue(Node node)
+            {
+                Node current = node;
+
+                while (current.left != null)
+                {
+                    current = current.left;
+                }
+                return (current.data);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            Node root = null;
+            root = tree.insert(root, 5);
+            tree.insert(root, 20);
+            tree.insert(root, 11);
+            tree.insert(root, 7);
+            tree.insert(root, 8);
+            tree.insert(root, 9);
+            Console.WriteLine("Min value is " + tree.minvalue(root));
+        }
+    }
+}
